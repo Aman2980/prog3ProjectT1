1c5baec28c8fbe1f0060d326066d1c17983c9e7c
Aman <100726793+Aman2980@users.noreply.github.com>
2023-10-05 15:52:09 +0000

    delted the project

--- prog3ProjectCycling/src/main/java/be/kdg/prog3/StartApplication.java
+++
@@ -1,9 +0,0 @@
-package be.kdg.prog3;
-import be.kdg.prog3.presenter.View;
-
-public class StartApplication {
-    public static void main(String[] args) {
-        View.startApplication();
-    }
-}
-
